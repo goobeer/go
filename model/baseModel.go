0774f768422c02e6313c184a7b3b03f4c6124591
goobeer <goobeer@sina.cn>
2017-03-03 17:35:39 +0800

    Update baseModel.go

--- model/baseModel.go
+++ model/baseModel.go
@@ -18,7 +18,7 @@ var (
 
 func init() {
 	var err error
-	engine, err = xorm.NewEngine("mysql", "root:watersa@tcp(127.0.0.1:3306)/tdb?charset=utf8")
+	engine, err = xorm.NewEngine("mysql", "dbUName:dbPwd@tcp(127.0.0.1:3306)/tdb?charset=utf8")
 	if err != nil {
 		common.PanicError(err)
 	}
